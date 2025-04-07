900097b0e5e3721dbf2a2314b7e1793be46d1a84
CatLocTeam <129519607+catlocteam@users.noreply.github.com>
2025-04-07 12:14:51 +0300

    Add files via upload

---
+++ public_html/script.js
@@ -0,0 +1,186 @@
+const translations = {
+  ua: {
+    // ! Main page
+
+    homePageText: "жіноча команда з локалізацій ігор",
+    homePageParag:
+      "ми працюємо день і ніч, щоб ви мали змогу грати українською",
+    contacts: "Контакти",
+    games: "iгри",
+    values: "цінності",
+    mono: "моно",
+    privat: "приват",
+    collaboration: "ЩоДо СПіВПРАЦі:",
+    "your-email": "ВАША ПОШТА...",
+    supportBtn: "images/support-btn.svg",
+
+    // ! Contact page
+    collaboration: "ЩоДо СПіВПРАЦі:",
+    question: "у вас є ПитаннЯ?",
+    "your-email": "ВАША ПОШТА...",
+    "your question": "ВАШЕ ПИТАННЯ...",
+    ourTeam: "Наша команда:",
+    search: "нараЗі ми Шукаємо:",
+    programmer: "- Програмісток",
+    smm: "- СММНИЦЬ",
+    textureArtist: "- ТЕКСТУРНИЦЬ",
+    communication: "- КОММУНіКАТОРОК",
+
+    // ! Values page
+    nationalizm: "українсЬкий наЦіонаЛіЗм",
+    feminism: "ФЕмініЗм",
+    discrimination: "віДсутністЬ ДискримінаЦії",
+
+    valueParagraph1:
+      "РОЗвиток наЦіОНАЛЬНОї свіДомості і україніЗаЦіЯ НАШОГО СУСПіЛЬСТВА Є НАДВАЖЛИВИМИ. НАША КОМАНДА З УСіХ СИЛ ПРАЦЮЄ НАД УКРАїНіЗАЦіЄЮ ігровОГО ПростОрУ. такоЖ МИ ДОНАТИМО на ЗСУ, воЛонтЕрам і ПостраЖДаЛим віД раШистсЬкої агрЕсії З коЖної сЕрії ВАШИХ БЛагоДійних ПОЖЕРТВ.",
+    valueParagraph2:
+      "ЦЯ команДа БУЛА СТВОРЕНА ЯК БЕЗПЕЧний ЖіноЧий Простір , БО МИ ВТОМИЛИСЯ ВіД УПЕРЕДЖЕНОГО СТАВЛЕННЯ У РОБОТі З ЧОЛОВіКАМИ. ТОМУ ОДНИМ З ГОЛОВНИХ НАШИХ ПРИОРИТЕТіВ Є  роЗвиток Жінок у ҐЕйм-інДустрії. Ми віДкрито ПіДтримуємо ФЕмініЗм, використовуємо ФЕмінітиви та вистуПАЄМО ПРОТИ МіЗОГІНії і СЛЮРів на аДрЕсу Жінок.",
+    valueParagraph3:
+      "НАША КОМАНДА нЕ ПіДтримує, ТА Й БіЛЬШЕ - КАТЕГОРИЧНО ПРОТИ ҐомоФОБії, СЕКСИЗМУ, ЕЙБЛіЗмУ, ЕЙДЖИЗМУ і БуДЬ-ЯкИХ іНШИХ ПроЯвіВ ДискримінаЦії. НатомістЬ МИ вПЕвнЕні, Що ДЛЯ КРАЩОГО МАЙБУТЬОГО, Україна має Бути комФортним місЦЕм ДЛЯ ВСіХ НАС, ДЕ ВСі ОДНАКОВО ПоЧувАТИМУТЬСЯ в БЕЗПЕЦі.",
+    // ! Game page
+    imgNewLife: "images/a_new_life_ua_sign.png",
+    textNewLife:
+      "Навіть якщо вам пощастило без труднощів знайти кохання, чи зможете ви його <span class='pink-text'>втримати</span>?",
+    localization: "ЛЕТСПЛЕї З НАШОЮ ЛОКАЛіЗАЦіЄЮ",
+    franBowImg: "images/fran_bow_ua_sign.png",
+    helpFran:
+      "Допоможіть маленькій <span style='color: black'>Френ боу</span> втекти з психлікарні й знайти її котика.. й вбивцю її батьків.",
+    wolfAmongUsImg: "images/thewoldamongustitl_ua.png",
+
+    wolfAmongUsText:
+      'разом з <span style="color:#ed208b">вовком біґбі</span> розкрийте справужорстокого вбивства, яке сколихнуло саме серце байкоміста ',
+    emmelineImg: "images/emmeline_ua.png",
+    emmelineText:
+      'Допоможіть маленькій <span style="color: black">Френ боу</span> втекти з психлікарні й знайти її котика.. й вбивцю її батьків.',
+    soon: "далі буде...",
+    findError: "ЗНАЙШЛИ ПОМИЛКУ?",
+    thanks: "Будемо раді будь-якому донату. Дякуємо за підтримку!",
+  },
+  en: {
+    // ! Main page
+    homePageText: "women’s team of game localisation",
+    homePageParag:
+      "we work day and night for ukrainians to enjoy your games in our native language",
+    contacts: "Contact",
+    games: "games",
+    values: "values",
+    mono: "mono",
+    privat: "privat",
+    collaboration: "For collaborations:",
+    "your-email": "Your email...",
+    supportBtn: "images/support-btn_eng.svg",
+
+    // ! Contact page
+    collaboration: "for collaborations:",
+    question: "Got any questions?",
+    "your-email": "Your email...",
+    "your question": "your question...",
+    ourTeam: "Our team:",
+    search: "We are currently <br> looking for:",
+    programmer: "- Programmer",
+    smm: "- SMM specialist",
+    textureArtist: "- Texture Artist",
+    communication: "- Communications Specialist",
+
+    // ! Values page
+
+    nationalizm: "ukrainian national identity",
+    feminism: "Feminism",
+    discrimination: "no discrimi <br> nation",
+
+    valueParagraph1:
+      "Returning to our cultural roots after centuries of oppression is extremely important. Our team is working hard to promote the Ukrainian language in the gaming space, where it didn't have a chance to exist in the not too distant past. We also donate to the Ukrainian Armed Forces, Ukrainian volunteers or victims of russian aggression from each series of charitable donations from our fans.",
+    valueParagraph2:
+      "We created this team to provide a safe space for women because we were tired of the bias we faced when working with men. So, one of our main goals is to help women grow in the games industry. We openly support feminism, use feminitives and stand against misogyny and slurs against women.",
+    valueParagraph3:
+      "Our team does not support and in fact has a firm policy against homophobia, sexism, ageism, abelism and any other form of discrimination. Instead, we believe that to build a better future, we need to create a comfortable place for all of us, where everyone is included and equally safe.",
+
+    // ! Game page
+    imgNewLife: "images/a_new_life_eng_sign.png",
+    textNewLife:
+      "Even if you are lucky enough to easily find a true love, will you be able <span class='pink-text'>to keep it</span>?",
+    localization: "Walkthrough with our localization",
+    franBowImg: "images/fran_bow_eng_sign.png",
+    helpFran:
+      " Help a young girl named <br/> <span style='color: black;'>Fran Bow</span> escape from a asylum and find her cat... And the killer of her parents.",
+    wolfAmongUsImg: "images/thewoldamongustitl_eng.png",
+    wolfAmongUsText:
+      ' Solve the case of a brutal murder that shook the very heart of the Fabletown together with <span style="color:#ed208b">Bigby Wolf</span>',
+    emmelineImg: "images/emmeline_eng.png",
+
+    emmelineText:
+      ' Help a young girl named <span style="color: black">Fran Bow</span> escape from a asylum and find her cat... And the killer of her parents.',
+    soon: "to be continued...",
+    findError: "Have found a problem?",
+    thanks: "We are grateful for every donation. Thank you for your support!",
+  },
+};
+
+function setLanguage(lang) {
+  localStorage.setItem("lang", lang);
+
+  document.querySelectorAll("[data-i18n]").forEach((el) => {
+    const key = el.getAttribute("data-i18n");
+    if (translations[lang][key]) {
+      el.innerHTML = translations[lang][key]; // Замість textContent
+    }
+    const text = translations[lang][key] || key;
+    el.innerHTML = text.replace(/\n/g, "<br>");
+  });
+
+  document.querySelectorAll("[data-i18n-placeholder]").forEach((el) => {
+    const key = el.getAttribute("data-i18n-placeholder");
+    el.placeholder = translations[lang][key] || key;
+  });
+
+  const supportBtn = document.querySelector(".support-btn");
+  if (supportBtn) {
+    const newImgSrc = translations[lang].supportBtn;
+    supportBtn.setAttribute("src", newImgSrc);
+  }
+
+  const imgNewLife = document.getElementById("newLife");
+  if (imgNewLife) {
+    imgNewLife.setAttribute("src", translations[lang].imgNewLife);
+  }
+  const franBowImg = document.getElementById("franBowImg");
+  if (franBowImg) {
+    franBowImg.setAttribute("src", translations[lang].franBowImg);
+  }
+  const wolfAmongUsImg = document.getElementById("wolfAmongUsImg");
+  if (wolfAmongUsImg) {
+    wolfAmongUsImg.setAttribute("src", translations[lang].wolfAmongUsImg);
+  }
+  const emmelineImg = document.getElementById("emmelineImg");
+  if (emmelineImg) {
+    emmelineImg.setAttribute("src", translations[lang].emmelineImg);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const savedLang = localStorage.getItem("lang") || "ua";
+
+  setLanguage(savedLang);
+
+  document
+    .getElementById("lang-ua")
+    .addEventListener("click", () => setLanguage("ua"));
+  document
+    .getElementById("lang-en")
+    .addEventListener("click", () => setLanguage("en"));
+});
+
+let burgerBtn = document.getElementById("burger-btn");
+let headerMobile = document.getElementById("header-mobile-menu");
+
+burgerBtn?.addEventListener("click", () => {
+  if (headerMobile.style.display === "block") {
+    headerMobile.style.display = "none";
+  } else {
+    headerMobile.style.display = "block";
+  }
+});
+
+document.getElementById("toggleDonate").addEventListener("click", function () {
+  document.querySelector(".donate-container").classList.toggle("show");
+});
